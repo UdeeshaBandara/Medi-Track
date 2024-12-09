f5bb42264313e094f370667c64e8239762d357b1
udeeshabandara <udeeshabandara@gmail.com>
2024-12-10 05:16:46 +0530

    buildspec and notification upated

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options:{
+        host: '0.0.0.0',
+        port: 4003
+      },
     logger: ['log', 'error', 'warn', 'debug', 'verbose'], 
-  });
-  // app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
-  // Logger.log('Application is running on: updated host name', 'Bootstrap');
+    },
+  );
+   app.listen();
+   Logger.log('Notification service is  listening on : 4003', 'Bootstrap');
 }
 bootstrap();
+
