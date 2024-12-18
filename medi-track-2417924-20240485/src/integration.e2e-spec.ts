03db7f1531a419648e57cf13b012c1edb2d2e853
udeeshabandara <udeeshabandara@gmail.com>
2024-12-19 00:29:38 +0530

    trigger 2

--- medi-track-2417924-20240485/src/integration.e2e-spec.ts
+++ medi-track-2417924-20240485/src/integration.e2e-spec.ts
@@ -1,9 +1,10 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { AppModule } from './app.module';
 import { AuthModule } from './auth/auth.module';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -11,7 +12,20 @@ describe('AppController (e2e)', () => {
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule, AuthModule],
+      imports: [AppModule, AuthModule,
+        ClientsModule.registerAsync([
+          {
+            name: 'PATIENTRECORD',
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.TCP,
+              options: {
+                host: configService.get<string>('PATIENT_RECORD_HOST', 'patient-record-blue'),
+                port: 4000
+              },
+            }),
+            inject: [ConfigService],
+          }
+        ])],
       providers: [AppService],
     }).compile();
 
@@ -22,7 +36,7 @@ describe('AppController (e2e)', () => {
 
   it('should check connection to the microservice', async () => {
     const response = await appService.checkTCPConnection();
-    expect(response).toBe('Healthy'); 
+    expect(response).toBe('Healthy');
   });
 
   afterAll(async () => {
