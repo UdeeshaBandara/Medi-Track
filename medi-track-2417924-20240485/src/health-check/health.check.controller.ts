6c4f65751b4944b09406a94b44fca389c94c2ea0
udeeshabandara <udeeshabandara@gmail.com>
2024-12-19 14:33:08 +0530

    change trigger

--- medi-track-2417924-20240485/src/health-check/health.check.controller.ts
+++ medi-track-2417924-20240485/src/health-check/health.check.controller.ts
@@ -21,7 +21,7 @@ export class HealthCheckController {
     
     const patientHost = this.configService.get<string>('PATIENT_RECORD_HOST');
  
-    return { message: 'Endpoint for health check', config: patientHost };
+    return { message: 'Health check endpoint', config: patientHost };
   }
 
 
